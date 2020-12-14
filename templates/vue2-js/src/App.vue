7532ec283742ad5dc817608ff9f2e9f6f1abf849
Pau Joan <pregonipaw@hotmail.com>
2020-12-14 19:54:17 +0100

    added vue2-js template.

---
+++ templates/vue2-js/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <RouterView />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue'
+import NavBar from './components/NavBar.vue'
+
+export default {
+  components: { NavBar, Footer }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ templates/vue2-js/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div class="home-logos scale-infinite">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <img
+        alt="Js logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+      />
+      <img
+        alt="Sass logo"
+        src="https://miro.medium.com/max/589/1*9U1toerFxB8aiFRreLxEUQ.png"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      message: 'Vue-js Paw & Bel template'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  min-height: $viewHeight;
+  @include flex();
+  flex-direction: column;
+  img {
+    width: 100px;
+    margin-right: 10px;
+  }
+}
+</style>
